80148fe6692fc839ffe93935a8e0aaf199ff0d34
r-nkgw <146305461+r-nkgw@users.noreply.github.com>
2025-07-24 11:06:28 +0900

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process': JSON.stringify({
+      env: { NODE_ENV: process.env.NODE_ENV || 'development' },
+      stdout: { isTTY: false }
+    })
+  }
 })
